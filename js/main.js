c64a73a2e108a1f78ebf0a00af4aaff5aefc14db
John Vilaikeo <jvilaikeo@gmail.com>
2017-05-22 09:40:31 -0700

    add wallet search

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$( document ).ready();
+
+$('#button').on('click', function(event) {
+   var wallet = $('#wallet').val();
+$.ajax({
+    url: "https://api.ethplorer.io/getAddressInfo/" + wallet  + "?apiKey=freekey",
+    // url: "https://api.ethplorer.io/getAddressInfo/0x64d141eecdee90e8cdee9a8601b957c0b2fba351" + "?apiKey=freekey",
+    type: 'GET',
+    dataType: 'json',
+    success: function(response){
+      console.log(response);
+
+      // if ( !response ){
+      //   // alert('error');
+      // $('wallet').text('Error: Not found').css("color", "red");
+      // }
+      // else {
+        var token = response.tokens["0"];
+        var tkInfo = response.tokens["0"].tokenInfo;
+      $('#address').text(tkInfo.address);
+      $('#name').text(tkInfo.name);
+      $('#balance').text(token.balance);
+    // }
+    },
+  });
+});
