56cd60816b35b7b93ba93aa085ce85e9854a9470
John Vilaikeo <jvilaikeo@gmail.com>
2017-05-22 21:52:55 -0700

    adjust pricing

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $( document ).ready();
 
+
 $('#button').on('click', function(event) {
    var wallet = $('#wallet').val();
 $.ajax({
@@ -24,14 +25,55 @@ $.ajax({
     },
   });
 });
+
 $.ajax({
     url: "https://api.gdax.com/products/BTC-USD/book",
     // url: "https://api.ethplorer.io/getAddressInfo/0x64d141eecdee90e8cdee9a8601b957c0b2fba351" + "?apiKey=freekey",
     type: 'GET',
     dataType: 'json',
     success: function(response){
-      console.log(response);
-      $('#btc-price').text(response.sequence.price);
+      console.log(response.asks);
+      var btc = response.asks;
+      // for (var items in btc){
+      //   $('#btc-price').text( btc[items]);
+      // }
+      $('#btc-price').text("BTC ask: " + "$" + btc["0"]["0"]);
+
     },
   });
+$.ajax({
+    url: "https://api.gdax.com/products/ETH-USD/book",
+    // url: "https://api.ethplorer.io/getAddressInfo/0x64d141eecdee90e8cdee9a8601b957c0b2fba351" + "?apiKey=freekey",
+    type: 'GET',
+    dataType: 'json',
+    success: function(response){
+      console.log(response.asks);
+      var eth = response.asks;
+      // for (var items in btc){
+      //   $('#btc-price').text( btc[items]);
+      // }
+      $('#eth-price').text("ETH ask: " + "$" + eth["0"]["0"]);
+
+    },
+   });
+$.ajax({
+
+    url: "https://api.gdax.com/products/ETH-USD/book",
+    // url: "https://api.ethplorer.io/getAddressInfo/0x64d141eecdee90e8cdee9a8601b957c0b2fba351" + "?apiKey=freekey",
+    type: 'GET',
+    dataType: 'json',
+    success: function(response){
+      console.log(response.asks);
+      var eth = response.asks;
+      // for (var items in btc){
+      //   $('#btc-price').text( btc[items]);
+      // }
+      $('#eth-price').text("ETH ask: " + "$" + eth["0"]["0"]);
+
+    },
+   });
+
+
+
+
 
